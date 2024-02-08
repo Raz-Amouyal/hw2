d29e255fb62c612b4e5691062fd7c5026b436a42
Raz-Amouyal <158087547+Raz-Amouyal@users.noreply.github.com>
2024-02-08 10:16:26 +0200

    Update calc_stat.sh

--- calc_stat.sh
+++ calc_stat.sh
@@ -5,22 +5,9 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
-# Flag to indicate if the course file is found
-course_found=false
+stats=$(ls -l | grep -o "$1".*)
 
-# Iterate over all files in the current directory
-for file in "$PWD/$1".*; do
-    # Check if the file matches the specified pattern
-    if [[ -e "$file" ]]; then
-        #arg $1 not necessary contain the grades' file name 
-    	stats="$file"
-        course_found=true
-        break
-    fi
-done
-
-# If course file is not found, print error message and exit
-if [[ $course_found == false ]]; then
+if [[ -z $stats ]]; then
     echo "Course not found" >&2
     exit 1
 fi
@@ -32,16 +19,15 @@ if [[ -d $directory_name ]]; then
 fi
 
 mkdir "$directory_name"
-#compile all the files
+
 gcc -g -Wall hist.c -o hist.exe
+
+./hist.exe "$stats" > ./"$directory_name"/histogram.txt
+
 gcc -g -Wall mean.c -o mean.exe
 gcc -g -Wall median.c -o median.exe
 gcc -g -Wall min.c -o min.exe
 gcc -g -Wall max.c -o max.exe
-
-#make hist
-./hist.exe "$stats" > ./"$directory_name"/histogram.txt
-
 mean=$(./mean.exe "$stats")
 
 median=$(./median.exe "$stats")
@@ -50,5 +36,4 @@ min=$(./min.exe "$stats")
 
 max=$(./max.exe "$stats")
 
-#combine the results with tab between
 echo -e "$mean\t$median\t$min\t$max" > ./"$directory_name"/statistics.txt
